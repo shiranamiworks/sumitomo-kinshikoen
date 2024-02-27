c0c52edf9b7258985d12f1320069929ced6dc96e
橋本真奈美 <manako.075h@gmail.com>
2024-02-27 14:55:26 +0900

    no message

--- html/js/location.js
+++ html/js/location.js
@@ -87,3 +87,16 @@ $(function () {
     arrows: false,
   });
 });
+
+$(function () {
+  if ($("#location").length) {
+    $(".change-buttons .button").on("click", function (e) {
+      e.preventDefault();
+      var _target = $(this).data("target");
+      $(".change-buttons .button").removeClass("current");
+      $(this).addClass("current");
+      $(".tab-inner").removeClass("current");
+      $(".tab-inner." + _target).addClass("current");
+    });
+  }
+});
