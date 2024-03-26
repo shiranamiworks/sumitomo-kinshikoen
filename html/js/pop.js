c1e052803011613bd6dda206038fb162f2524c4a
福井彩佳 <ayaka.fsp@gmail.com>
2024-03-26 16:17:14 +0900

    no message

--- html/js/pop.js
+++ html/js/pop.js
@@ -1,18 +1,18 @@
 
 function outline(){
-	sub = window.open("/shuto/kinshikoen/detail.cgi","","toolbar=no,location=no,directories=no,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=780,height=580");
+	sub = window.open("/shuto/kinshikouen/detail.cgi","","toolbar=no,location=no,directories=no,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=780,height=580");
 }
 
 function outline1(){
-	sub = window.open("/shuto/kinshikoen/detail.cgi?T=1","","toolbar=no,location=no,directories=no,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=780,height=580");
+	sub = window.open("/shuto/kinshikouen/detail.cgi?T=1","","toolbar=no,location=no,directories=no,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=780,height=580");
 }
 
 function outline2(){
-	sub = window.open("/shuto/kinshikoen/detail.cgi?T=2","","toolbar=no,location=no,directories=no,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=780,height=580");
+	sub = window.open("/shuto/kinshikouen/detail.cgi?T=2","","toolbar=no,location=no,directories=no,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=780,height=580");
 }
 
 function blog(){
-	sub = window.open("/shuto/kinshikoen/blog/","");
+	sub = window.open("/shuto/kinshikouen/blog/","");
 }
 
 
