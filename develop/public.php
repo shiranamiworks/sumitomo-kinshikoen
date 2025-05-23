c0b15cbab66cc068228eba7a915a5f440909954a
橋本真奈美 <manako.075h@gmail.com>
2025-05-23 18:19:18 +0900

    no message

---
+++ develop/public.php
@@ -0,0 +1,68 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "design",
+  "gnav_set" => "design",
+  "page_class" => "design",
+  "url" => "design.html",
+  "title" => "デザイン｜",
+  "keywords" => "デザイン,",
+  "description" => "デザインページ。",
+  "add_stylesheet" => ["css/design.css"],
+  "add_script" =>  ["js/design.js"],
+  "canonical_page_id" => "design", 
+  "logo_text" => "デザイン｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー錦糸公園｜錦糸町 マンション｜トップ</a></li>
+      <li><p>デザイン</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/design/mv.jpg" alt="" class="pc"><img src="./images/design/mv-sp.jpg" alt="" class="tb">
+   <p class="annotation white">外観完成予想図（1）</p>
+  </header><!-- main-visual -->
+
+
+
+  <section class="sec01 sections clearfix">
+  <div class="inner">
+    <p class="tit">FACADE DESIGN</p>
+    <p class="text">洗練された存在感示す、街ゆく人々を魅了する佇まい</p>
+    <p class="lead">重厚感のあるデザインを纏い、街の景観に際立つ新たな象徴となるファサード。</p>
+    </div><!-- inner -->
+    <div class="image">
+      <img src="./images/design/img01.jpg" alt="">
+      <p class="annotation white">エントランス完成予想図</p>
+    </div>
+  </section><!-- sec01 -->
+
+
+
+  <section class="sec02 sections clearfix">
+    <div class="image">
+      <img src="./images/design/img02.jpg" alt="">
+      <p class="annotation white">エントランス完成予想図</p>
+      <p class="txt">水と緑と空とまち。<br>華やかに暮らし潤う日々へ。</p>
+    </div>
+  </section><!-- sec02 -->
+
+
+
+<div class="annotation-area">
+  <p class="page-annotation js-scroll wrap clearfix">※掲載の外観完成予想図?は計画段階の図面を基に描き起こしたもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。なお、周辺環境は2021年5月時点の都市データを基に描き起こしたもので、実際とは多少異なります。また、周辺環境・眺望は将来変わる場合がございます。一部敷地外（道路・河川等）を合わせて着彩しています。また周辺建物は概略ラインのみ表現しています。建物等の状況がわかりやすいよう、電柱等を透視して描いています。敷地内の樹木等はある程度生育した後をイメージして描いたものです。また、葉や花の色合い、樹形等はイメージであり実際とは異なります。なお、植栽計画は変更される場合がありますので、あらかじめご了承ください。<br>※掲載のエントランス完成予想図は計画段階の図面を基に描き起こしたものに車等をCG合成したもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。敷地内の樹木等はある程度生育した後をイメージして描いたものです。また、葉や花の色合い、樹形等はイメージであり実際とは異なります。なお、植栽計画は変更される場合がありますので、あらかじめご了承ください。<br>※掲載の外観完成予想図?は、現地15階相当から北西方面を撮影した周辺写真(2023年12月撮影）に、計画段階の図面を基に描き起こした建物完成予想図をCG合成・加工したもので、実際とは異なります。また、形状の細部および設備機器等については省略しております。周辺環境・眺望は将来変わる場合があります。また、季節・天候によっても異なる場合があります。家具・調度品等オプション仕様は販売価格に含まれません。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
