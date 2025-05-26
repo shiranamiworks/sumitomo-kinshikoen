dafa538f4fedeac87481596321802790a5a0f2c4
福井彩佳 <ayaka.fsp@gmail.com>
2025-05-26 12:08:56 +0900

    no message

--- develop/equipment02.php
+++ develop/equipment02.php
@@ -42,138 +42,99 @@
     <div class="wrap">
       <h2 class="tit">健康・エコ・省エネ</h2>
       <ul class="lists list00">
-        <li class="half">
-        <p class="tit-img"><span>外気を常に取り入れる</span>24時間常時小風量換気システム</p>
-        <p class="lead">窓を閉めたままでも常に換気ができる、24時間常時小風量換気システムを採用しています。各室の自然給気口から外気を取り入れ、ドア下を経由して、バスルーム、パウダールーム、トイレの換気口から室内の空気を排出します。</p>
-        </li>
-        <li class="half mr0">
-          <p class="tit-img"><span>大阪ガスの高効率ガス給湯器</span>エコジョーズ</p>
-          <div class="text">
-            <p class="lead">環境と家計の両方に優しい大阪ガスの高効率ガス給湯器「エコジョーズ」を採用。キッチン、バスルーム、パウダールームへのスムーズな給湯はもちろん、床暖房や浴室暖房乾燥機までをトータルにサポートします。</p>
-          </div>
+        <li>
+          <p class="tit-img">24時間常時小風量換気システム</p>
+          <p class="lead">窓を閉めたままでも常に換気ができる、24時間常時小風量換気システムを採用しています。各室の自然給気口（花粉フィルター付）から外気を取り入れ、ドア下を経由して、バスルーム、パウダールーム、トイレの換気口から室内の空気を排出します。</p>
+          <p class="tit-img mt30">生ゴミディスポーザー</p>
+          <p class="lead">生ゴミを、キッチンですばやく粉砕処理できるシステムです。各住戸のディスポーザーで破砕された生ゴミは、専用の排水処理装置で浄化してから排水することができます。<span>※一部処理できないものもあります。</span></p>
           <div class="image">
-            <img src="./images/equipment02/img01.png" alt="" class=""><p class="annotation gothic">参考写真2</p>
+            <img src="./images/equipment02/img01.png" alt="" class=""><p class="annotation left gothic">（概念図）</p><p class="annotation gothic">参考写真1</p>
+          </div>
+        </li>
+        <li>
+          <p class="tit-img">エコジョーズ</p>
+          <p class="lead">環境と家計の両方に優しい東京ガスの高効率ガス給湯器「エコジョーズ」を採用。キッチン、バスルーム、パウダールームへのスムーズな給湯はもちろん、床暖房や浴室暖房乾燥機までをトータルにサポートします。</p>
+          <div class="image">
+            <img src="./images/equipment02/img02.png" alt="" class=""><p class="annotation left gothic">参考写真2</p>
+          </div>
+          <p class="lead mt30"><span>※1 FT4215ARSAW6Qの場合（スタンダードの一例）※2 FT4220・FT4222シリーズの場合（エコジョーズの一例）※1※2はJIS S 2109で定める測定方法により算定 ただし※1～2ともご使用状況により熱効率は異なります。※3 試算条件：ガス使用量は季節に応じて変化するため、家庭用の平均的な月別使用量比率に従って年間ガス使用量を各月に割り付けたうえで試算。居間の暖房はガス温水床暖房と電気エアコンを併用、居間以外の暖房及び冷房は電気エアコン使用、浴室暖房乾燥機使用（衣類乾燥週2回/年、浴室暖房週7回/冬期）。機器の運転状況等により削減量は異なります。機器の運転状況により熱源機の熱効率は変動するため、熱効率を一定にしたうえで試算。CO2排出係数はガス2.29kg-CO2/m<sup>2</sup>（東京ガスデータ）</span></p>
+        </li>
+        <li>
+        <p class="tit-img">節水シャワーヘッド</p>
+        <p class="lead">シャワーヘッドに付いたボタンを押すだけで、吐水・止水を繰り返すことができ大変便利です。また、従来のシャワーと比較すると最大約44％＜パナソニック㈱比＞節水となります。</p>
+        </li>
+        <li>
+        <p class="tit-img">ホルムアルデヒド対策</p>
+        <p class="lead">建材や内装材などの素材（建築基準法上の特定建材）には、ＪＩＳやＪＡＳによって定められたホルムアルデヒドの発散量が少ないものを採用。健康的な住まいを追求しています。</p>
+        </li>
+        <li>
+          <p class="tit-img">エコ機能付き給湯リモコン</p>
+          <div class="text01">
+            <p class="lead">キッチンとバスルームの給湯リモコンには「エコ運転機能」を搭載。画面メニューから「エコ」をタッチするだけで給湯量が自動調整でき、省エネ・節約に効果的です。また、ＴＥＳ熱源機（エコジョーズ）で使用した一日のガス・お湯の量、使用料金の目安、ＣO2の排出量を表示する「エネルック機能」も付いています。<span>※「エコ運転機能」を利用して2ヶ所以上でお湯を使うと湯細りする可能性があります。</span></p>
+          </div>
+          <div class="image image01">
+            <img src="./images/equipment02/img03.png" alt="" class="img03"><p class="annotation gothic">参考写真2</p>
+          </div>
+        </li>
+        <li>
+          <p class="tit-img">Low-Eガラス</p>
+          <div class="text01">
+            <p class="lead">開口部には省エネ効果に優れたLow-Eガラスを採用。ガラスの表面にコーティングされた日射熱の反射性を高める特殊金属膜（Low-E膜）と断熱性を高める空気層により、冷暖房両方の負荷を軽減します。<span>※詳細は係員にお尋ねください。</span></p>
+          </div>
+          <div class="image image01">
+            <img src="./images/equipment02/img04.png" alt=""></p><p class="annotation gothic">（概念図）</p>
           </div>
         </li>
         <li class="full">
-          <p class="tit-img"><span>クリーンな暖房システムの</span>大阪ガス ヌック温水床暖房</p>
+          <p class="tit-img">TES温水床暖房</p>
           <div class="text01">
-            <p class="lead">リビング・ダイニングには、大阪ガスのヌック温水床暖房を採用。温水を利用して足元から心地よく室内を暖め、理想的といわれる『頭寒足熱』を実現する暖房システムです。</p>
+            <p class="lead">リビング・ダイニングおよびリビング・ダイニングに隣接する洋室の1室には、東京ガスのTES温水床暖房を採用。温水を利用して足元から心地よく室内を暖め、理想的といわれる『頭寒足熱』を実現する暖房システムです。</p>
           </div>
           <div class="image01">
             <div class="image img01">
             <p class="tit-img"><span>床暖房の垂直温度分布</span></p>
-              <img src="./images/equipment02/img02-01.jpg" alt="" class=""><p class="annotation gothic">※参考図（天井高は実際とは異なります。）</p>
+              <img src="./images/equipment02/img05-01.jpg" alt="" class=""><p class="annotation gothic">※参考図（天井高は実際とは異なります。）</p>
             </div>
             <div class="image img02">
-              <img src="./images/equipment02/img02-02.jpg" alt="" class=""><p class="annotation gothic">参考写真2</p>
+              <img src="./images/equipment02/img05-02.jpg" alt="" class=""><p class="annotation gothic">参考写真2</p>
             </div>
           </div>
         </li>
         <li class="full">
-          <p class="tit-img"><span>生活にもお肌にも潤いを与える</span>ミストサウナ</p>
+          <p class="tit-img">ミストサウナ</p>
           <p class="lead">一般のドライサウナと比べ、低温・高湿度で息苦しくなくリラックスして楽しめます。気分をリフレッシュしたいとき、お肌をやさしくいたわりたいとき、ミストサウナ入浴で素敵なバスタイムを。さらに、衣類のリフレッシュや浴室のカビ抑制により、家事の負担を軽減します。</p>
-          <div class="image full">
-            <img src="./images/equipment02/img03.jpg" alt="" class="pc"><img src="./images/equipment02/img03-sp.jpg" alt="" class="tb">
+          <div class="image full mt30">
+            <img src="./images/equipment02/img06.jpg" alt="">
           </div>
         </li>
       </ul>
       <ul class="lists list01">
-        <li>
-          <p class="tit-img"><span>簡単に吐水・止水できる</span>節水シャワーヘッド</p>
-          <p class="lead">シャワーヘッドに付いたボタンを押すだけで、吐水・止水を繰り返すことができ大変便利です。また、スプレー式( 節水) のシャワーヘッドなので従来品と比較すると最大約48％＜㈱LIXIL比＞節水効果もあります。</p>
-        </li>
-        <li>
-          <p class="tit-img"><span>シックハウス症候群に配慮した</span>ホルムアルデヒド対策</p>
-          <p class="lead">建材や内装材などの素材（建築基準法上の特定建材）には、ＪＩＳやＪＡＳによって定められたホルムアルデヒドの発散量が少ないものを採用。健康的な住まいを追求しています。</p>
-        </li>
-        <li>
-         <p class="tit-img"><span>快適な室温を保つ</span>断熱構造</p>
-          <p class="lead">天井および外気に面する柱・梁・壁の室内側に約20㎜～約45㎜厚、最下階住戸の床下に約50㎜～約100㎜厚の断熱材を施工。また、屋上には約50㎜厚の断熱材を施しています。<span>※断熱材施工範囲は各住戸により異なります。詳細は係員にお尋ねください。</span></p>
-        </li>
-      </ul>
-      <ul class="lists list02">
-      <li>
-        <p class="tit-img"><span>高い断熱性能を備えた</span>Low-Eガラス</p>
-        <p class="lead">開口部には省エネ効果に優れたLow-Eガラスを採用。ガラスの表面にコーティングされた日射熱の反射性を高める特殊金属膜（Low-E膜）と断熱性を高める空気層により、冷暖房両方の負荷を軽減します。<br><span>※詳細は係員にお尋ねください。</span></p>
-          <div class="image tb">
-            <img src="./images/equipment02/img05.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
-        <li>
-          <p class="tit-img"><span>真空層で高断熱化を実現</span>真空ガラス</p>
-          <p class="lead">一部住戸の開口部には、断熱性能に優れた真空ガラスを使用しています。真空ガラスは2枚のガラスの間に真空層をつくることで、熱の「伝導」と「対流」を防ぎ、「放射」は特殊金属膜で抑えています。そのため冬は暖房負荷を軽減し、夏は日射熱を反射して「冬は暖かく、夏は涼しい」環境をつくりだします。</span></p>
-          <div class="image tb">
-            <img src="./images/equipment02/img06.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
-        <li>
-          <p class="tit-img"><span>断熱性・耐久性に配慮した</span>外壁</p>
-          <p class="lead">外壁のコンクリート厚は、約150㎜～約180㎜を確保し、室内側には断熱材を吹き付けて、省エネにも配慮しています。</p>
-          <div class="image tb">
-            <img src="./images/equipment02/img04.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
-        <li>
-          <div class="image pc">
-            <img src="./images/equipment02/img04.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
-        <li>
-          <div class="image pc">
-            <img src="./images/equipment02/img05.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
-        <li>
-          <div class="image pc">
-            <img src="./images/equipment02/img06.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
-       <li>
-        <p class="tit-img"><span>環境にやさしいクリーンエネルギー</span>太陽光発電システム</p>
-        <p class="lead">自然エネルギーを有効に活用した太陽光発電システムを屋上に設置。マンションの共用部で使用する電力の一部を賄う事ができます。</p>
-          <div class="image tb">
-            <img src="./images/equipment02/img07.jpg" alt="" class="">
-            <p class="annotation gothic">参考写真2</p>
-          </div>
-        </li>
-        <li>
-          <p class="tit-img"><span>無駄な湯水を省く</span>節湯水栓</p>
+        <li class="double">
+          <p class="tit-img">節湯水栓</p>
           <p class="lead">キッチン・パウダールームなどの水栓で湯水を使用する際、必要以上の流量で湯水を使用すると、水の消費量だけでなく、給湯のためのエネルギー消費量も増大します。水と湯水の境目にある切替ポイントでスマートにエコを意識し、効率的に無駄な湯水を省くことのできる節湯水栓を採用しています。</p>
-          <div class="image tb">
-            <img src="./images/equipment02/img08.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
+          <div class="image mt30">
+            <img src="./images/equipment02/img07.png" alt="" class="pc"><img src="./images/equipment02/img07-sp.png" alt="" class="tb"><p class="annotation left02 gothic">（概念図）</p><p class="annotation gothic">（概念図）</p>
           </div>
+          <p class="tit-img mt30">LED照明</p>
+          <p class="lead">キッチン・パウダールームなどの水栓で湯水を使用する際、必要以上の流量で湯水を使用すると、水の消費量だけでなく、給湯のためのエネルギー消費量も増大します。水と湯水の境目にある切替ポイントでスマートにエコを意識し、効率的に無駄な湯水を省くことのできる節湯水栓を採用しています。</p>
         </li>
         <li>
-          <p class="tit-img"><span>消費電力量を削減する</span>LED照明</p>
-          <p class="lead">住戸内のダウンライトにはLED照明を採用。従来の白熱灯に比べ寿命が長持ちし、消費電力量が削減されます。</p>
-        </li>
-        <li>
-          <div class="image pc">
-            <img src="./images/equipment02/img07.jpg" alt="" class="">
-            <p class="annotation gothic">参考写真2</p>
+          <p class="tit-img">太陽光発電システム</p>
+          <p class="lead">自然エネルギーを有効に活用した太陽光発電システムを屋上に設置。マンションの共用部で使用する電力の一部を賄う事ができます。</p>
+          <div class="image">
+              <img src="./images/equipment02/img08.jpg" alt="" class=""><p class="annotation gothic">参考写真2</p>
           </div>
+          <p class="tit-img mt30">断熱構造</p>
+          <p class="lead">天井・床および外気に面する柱・梁・壁の室内側に約20㎜～約40㎜厚、最下階住戸の床下に約75㎜厚の断熱材を施工。また、屋上には約50㎜厚の断熱材を施しています。<span>※断熱材施工範囲は各住戸により異なります。 詳細は係員にお尋ねください。</span></p>
         </li>
-        <li>
-          <div class="image pc">
-            <img src="./images/equipment02/img08.jpg" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
-          </div>
-        </li>
+      </ul>   
     </div>
   </section><!-- sec01 -->
 
 
 
   <div class="annotation-area">
-  <p class="page-annotation js-scroll wrap clearfix">※掲載の参考写真2はメーカーカタログ等の参考写真であり本物件のものではありません。※設備・仕様はタイプにより異なります。</p></div>
+  <p class="page-annotation js-scroll wrap clearfix">※掲載の参考写真1は本物件と同様仕様のコンセプトルーム（総合マンションギャラリー新橋館）を撮影（2023年1月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれておりません<br>※掲載の参考写真2はメーカーカタログ等の参考写真であり本物件のものではありません。※設備・仕様はタイプにより異なります。</p></div>
 </main>
 
   <div class="equipment-link equipment-link--5">
