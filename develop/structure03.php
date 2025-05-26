26aedfa6fd37f08e7d0212aac1e9fc738b71c9b9
yoshino umemi <umemi.0401@gmail.com>
2025-05-26 20:11:39 +0900

    no message

--- develop/structure03.php
+++ develop/structure03.php
@@ -3,12 +3,12 @@
   "directory_pos" => "./",
   "page_id" => "structure",
   "gnav_set" => "structure",
-  "page_class" => "structure02",
-  "url" => "structure02.html",
+  "page_class" => "structure03",
+  "url" => "structure03.html",
   "title" => "セキュリティ｜",
   "keywords" => "セキュリティ,",
   "description" => "セキュリティページ。",
-  "add_stylesheet" => ["css/structure.css","css/structure02.css"],
+  "add_stylesheet" => ["css/structure.css","css/structure03.css"],
   "add_script" =>  [],
   "canonical_page_id" => "structure", 
   "logo_text" => "セキュリティ｜"
@@ -22,7 +22,7 @@
   <div class="breadcrumb sp-hide">
     <ul>
       <li><a href="./index.html">シティタワー錦糸公園｜錦糸町 マンション｜トップ</a></li>
-      <li><a href="./structure02.html">構造・防災</a></li>
+      <li><a href="./structure01.html">構造・防災</a></li>
       <li><p>セキュリティ</p></li>
     </ul>
   </div>
@@ -41,7 +41,7 @@
           <p class="tit-img"><span>住友不動産の24時間オンラインセキュリティシステム</span>S-GUARD（エスガード）</p>
           <p class="lead">24時間オンライン体制で、セントラル警備保障（株）（CSP）指令センターにつながっています。各住戸、共用部で警報が発信されると、セントラル警備保障（株）の警備員が現場に急行し、必要な通報などの対応を致します。また、警備業務明細書の規定に従い、事前にお預かりした専有部の鍵を使用した住戸内での対応、共用部設備の異常信号を受信した場合の現地確認等、迅速かつ適切な対処に貢献します。</p>
           <div class="image">
-            <img src="./images/structure02/img01.png" alt="" class="">
+            <img src="./images/structure03/img01.png" alt="" class="">
             <p class="annotation gothic bt">（概念図）</p>
           </div>
           <p class="tit-img"><span>安心で快適な</span>エレベーター内の防犯対策</p>
@@ -51,12 +51,12 @@
           <p class="tit-img"><span>来訪者を3段階でチェックして不審者の侵入対策を強化</span>ダブルオートロックシステム</p>
           <p class="lead">一般のマンションに比べ不審者の侵入対策を強化し、主な来訪者のアプローチ上の2ヶ所にオートロックシステムを採用しました。住戸前のセキュリティを加え3段階のチェックで、不審者の侵入対策を強化しています。また留守中の来訪者も確認できる録画録音機能もついています。</p>
           <div class="image">
-            <img src="./images/structure02/img02.png" alt="" class=""><p class="annotation bt003 gothic">（概念図）</p>
+            <img src="./images/structure03/img02.png" alt="" class=""><p class="annotation bt003 gothic">（概念図）</p>
           </div>
           <p class="tit-img"><span>共用部の防犯性に配慮した</span>防犯カメラ</p>
           <p class="lead">エントランスやエレベーター内、外構等の共用部に防犯カメラを設置。不審者の侵入や犯罪を抑制するとともに、24時間録画される画像は一定期間保管されます。<span>※エレベーター以外はレンタルとなり、費用等は管理費に含まれます。</span></p>
           <div class="image wid75">
-            <img src="./images/structure02/img03.png" alt="" class=""><p class="annotation gothic">参考写真2</p>
+            <img src="./images/structure03/img03.png" alt="" class=""><p class="annotation gothic">参考写真2</p>
           </div>
         </li>
       </ul>
@@ -66,36 +66,36 @@
           <p class="tit-img font18"><span>玄関前にいる来訪者を映像でチェックできる</span>カメラ付玄関前インターホン（子機）</p>
            <p class="lead">玄関前にいる来訪者を、音声に加え住戸内のカラーモニターより映像でも確認できるカメラ付インターホンを採用。また留守中の来訪者も確認できる録画・録音機能もついています。</p>
            <div class="image tb">
-            <img src="./images/structure02/img04.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img04.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
            </div>
         </li>
         <li>
           <p class="tit-img"><span>暴力的なドア破壊に対応</span>鎌デッド錠</p>
            <p class="lead">枠と扉の間にバールなどを差し込んで解錠する、こじ開けの手口への対策として、玄関ドアのカギを施錠すると鎌状の金具がせりだす鎌デッド錠を採用しました。</p>
            <div class="image tb">
-            <img src="./images/structure02/img05.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img05.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
            </div>
         </li>
         <li>
           <p class="tit-img"><span>使いやすさと防犯性を兼ね備えた</span>防犯サムターン</p>
           <p class="lead">工具をドアの内側に入れサムターンを回してしまう不正解錠に対応したスイッチ式防犯サムターン（上部1ヶ所）を採用しました。</p>
           <div class="image tb">
-            <img src="./images/structure02/img06.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img06.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
            </div>
         </li>
         <li>
            <div class="image wid75 pc">
-            <img src="./images/structure02/img04.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img04.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
            </div>
         </li>
         <li>
            <div class="image wid75 pc">
-            <img src="./images/structure02/img05.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img05.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
            </div>
         </li>
         <li>
            <div class="image wid75 pc">
-            <img src="./images/structure02/img06.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+            <img src="./images/structure03/img06.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
            </div>
         </li>
         <li>
@@ -103,7 +103,7 @@
           <div class="lead-box">
             <p class="lead">防犯性を高めるため、玄関ドアは上下2ヶ所で施錠できるダブルロック仕様としました。</p>
             <div class="image">
-              <img src="./images/structure02/img07.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+              <img src="./images/structure03/img07.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
             </div>
           </div>
         </li>
@@ -112,7 +112,7 @@
           <div class="lead-box">
             <p class="lead lead01">玄関ドア（2階各タイプバルコニー側窓、3階Aタイプ・3階Dタイプバルコニー側窓）には防犯センサーを設置（※面格子の付く窓とFIX窓を除く）。防犯セットしている間に防犯センサーが設置された開口部が開けられると、センサーが反応し、住戸内インターホン（親機）と玄関前インターホン（子機）より警報が鳴り、室内外に異常をお知らせするとともに、警備会社、住友不動産建物サービス設備管理センターへ自動通報されます。</p>
             <div class="image img01">
-              <img src="./images/structure02/img08.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
+              <img src="./images/structure03/img08.jpg" alt="" class=""><p class="annotation gothic">参考写真1</p>
             </div>
           </div>
         </li>
