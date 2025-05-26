f7d27f71bbe658bd784ba409285f40a4fe2c86c6
橋本真奈美 <manako.075h@gmail.com>
2025-05-26 13:28:17 +0900

    no message

--- develop/public.php
+++ develop/public.php
@@ -1,17 +1,17 @@
 <!doctype html><?php
  $page_settings = array(
   "directory_pos" => "./",
-  "page_id" => "design",
-  "gnav_set" => "design",
-  "page_class" => "design",
-  "url" => "design.html",
-  "title" => "デザイン｜",
-  "keywords" => "デザイン,",
-  "description" => "デザインページ。",
-  "add_stylesheet" => ["css/design.css"],
-  "add_script" =>  ["js/design.js"],
-  "canonical_page_id" => "design", 
-  "logo_text" => "デザイン｜"
+  "page_id" => "public",
+  "gnav_set" => "public",
+  "page_class" => "public",
+  "url" => "public.html",
+  "title" => "共用空間｜",
+  "keywords" => "共用空間,",
+  "description" => "共用空間ページ。",
+  "add_stylesheet" => ["css/public.css"],
+  "add_script" =>  ["js/public.js"],
+  "canonical_page_id" => "public", 
+  "logo_text" => "共用空間｜"
 );
 ?>
 
@@ -22,39 +22,49 @@
   <div class="breadcrumb sp-hide">
     <ul>
       <li><a href="./index.html">シティタワー錦糸公園｜錦糸町 マンション｜トップ</a></li>
-      <li><p>デザイン</p></li>
+      <li><p>共用空間</p></li>
     </ul>
   </div>
 	<!--// パンくずリスト -->
 
   <header class="main-visual clearfix">
     <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
-   <div class="image"><img src="./images/design/mv.jpg" alt="" class="pc"><img src="./images/design/mv-sp.jpg" alt="" class="tb">
-   <p class="annotation white">外観完成予想図（1）</p>
+   <div class="image"><img src="./images/public/mv.jpg" alt="" class="pc"><img src="./images/public/mv-sp.jpg" alt="" class="tb">
+   <p class="annotation white">車寄せ完成予想図</p>
   </header><!-- main-visual -->
 
 
 
   <section class="sec01 sections clearfix">
-  <div class="inner">
-    <p class="tit">FACADE DESIGN</p>
-    <p class="text">洗練された存在感示す、街ゆく人々を魅了する佇まい</p>
-    <p class="lead">重厚感のあるデザインを纏い、街の景観に際立つ新たな象徴となるファサード。</p>
+    <div class="inner">
+      <p class="tit">ENTRANCE</p>
+      <p class="text">風格と気品が漂うエントランス</p>
+      <p class="lead">街に調和しながらも際立つ堂々とした佇まい。<br>ホテルライクな車寄せが訪れる人を優雅にエスコートします。</p>
     </div><!-- inner -->
     <div class="image">
-      <img src="./images/design/img01.jpg" alt="">
-      <p class="annotation white">エントランス完成予想図</p>
+      <img src="./images/public/img01.jpg" alt="">
+      <p class="annotation white">エントランスホール完成予想図</p>
     </div>
   </section><!-- sec01 -->
 
 
 
   <section class="sec02 sections clearfix">
+    <div class="inner">
+      <p class="tit">LAND PLAN</p>
+      <p class="text">配棟の工夫で贅なる空間を創出</p>
+      <p class="lead">前面道路と建物を並行に配置しないことで敷地を有効的に活用しながら<br>ゆとりのアプローチや車寄せを実現したランドプラン。</p>
+    </div><!-- inner -->
     <div class="image">
-      <img src="./images/design/img02.jpg" alt="">
-      <p class="annotation white">エントランス完成予想図</p>
-      <p class="txt">水と緑と空とまち。<br>華やかに暮らし潤う日々へ。</p>
+      <img src="./images/public/img02.png" alt="">
+      <p class="annotation white">敷地配置概念図</p>
     </div>
+      <p class="txt">景観を彩る植栽計画</p>
+      <ul class="list">
+        <li>
+
+        </li>
+      </ul>
   </section><!-- sec02 -->
 
 
