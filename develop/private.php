d5ede44eae84b89189356fe54aa0cf84b4f6f45c
橋本真奈美 <manako.075h@gmail.com>
2025-05-26 16:02:53 +0900

    no message

--- develop/private.php
+++ develop/private.php
@@ -1,17 +1,17 @@
 <!doctype html><?php
  $page_settings = array(
   "directory_pos" => "./",
-  "page_id" => "design",
-  "gnav_set" => "design",
-  "page_class" => "design",
-  "url" => "design.html",
-  "title" => "デザイン｜",
-  "keywords" => "デザイン,",
-  "description" => "デザインページ。",
-  "add_stylesheet" => ["css/design.css"],
-  "add_script" =>  ["js/design.js"],
-  "canonical_page_id" => "design", 
-  "logo_text" => "デザイン｜"
+  "page_id" => "private",
+  "gnav_set" => "private",
+  "page_class" => "private",
+  "url" => "private.html",
+  "title" => "室内空間｜",
+  "keywords" => "室内空間,",
+  "description" => "室内空間ページ。",
+  "add_stylesheet" => ["css/private.css"],
+  "add_script" =>  ["js/private.js"],
+  "canonical_page_id" => "private", 
+  "logo_text" => "室内空間｜"
 );
 ?>
 
@@ -22,45 +22,67 @@
   <div class="breadcrumb sp-hide">
     <ul>
       <li><a href="./index.html">シティタワー錦糸公園｜錦糸町 マンション｜トップ</a></li>
-      <li><p>デザイン</p></li>
+      <li><p>室内空間</p></li>
     </ul>
   </div>
 	<!--// パンくずリスト -->
 
   <header class="main-visual clearfix">
     <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
-   <div class="image"><img src="./images/design/mv.jpg" alt="" class="pc"><img src="./images/design/mv-sp.jpg" alt="" class="tb">
-   <p class="annotation white">外観完成予想図（1）</p>
+   <div class="image"><img src="./images/private/mv.jpg" alt="" class="pc"><img src="./images/private/mv-sp.jpg" alt="" class="tb">
+   <p class="annotation white">眺望写真</p>
+   </div>
+   <p class="mini">※掲載の眺望写真は現地18階相当からの眺望写真（2023年12月撮影）。 <br>※眺望等は階数・各住戸により異なり、周辺環境・眺望は将来変わる場合があります。季節・天候によっても異なる場合があります。</p>
   </header><!-- main-visual -->
 
 
 
   <section class="sec01 sections clearfix">
   <div class="inner">
-    <p class="tit">FACADE DESIGN</p>
-    <p class="text">洗練された存在感示す、街ゆく人々を魅了する佇まい</p>
-    <p class="lead">重厚感のあるデザインを纏い、街の景観に際立つ新たな象徴となるファサード。</p>
+    <p class="tit">PRIVATE SPACE</p>
+    <p class="text">快適な住空間が心地よい日々の暮らしをサポート</p>
     </div><!-- inner -->
-    <div class="image">
-      <img src="./images/design/img01.jpg" alt="">
-      <p class="annotation white">エントランス完成予想図</p>
-    </div>
   </section><!-- sec01 -->
 
 
 
   <section class="sec02 sections clearfix">
-    <div class="image">
-      <img src="./images/design/img02.jpg" alt="">
-      <p class="annotation white">エントランス完成予想図</p>
-      <p class="txt">水と緑と空とまち。<br>華やかに暮らし潤う日々へ。</p>
-    </div>
+    <div class="inner">
+      <div class="wrap">
+      <p class="tit">広がりのある住空間を創出</p>
+      <p class="txt">アウトフレーム設計</p>
+      <p class="lead">躯体柱をリビング・ダイニング、洋室の外に出したアウトフレーム設計を採用しました。家具のレイアウトなどスペースが有効活用できます。</p>
+      <p class="mini">※室内の壁の形状は概念図上では一部省略しています。詳細は図面集をご確認ください。<br>※主開口部側のみ。</p>
+      <div class="image">
+      <img src="./images/private/img01.png" alt="">
+      </div>
+      </div><!-- wrap -->
+    </div><!-- inner -->
   </section><!-- sec02 -->
 
 
 
+  <section class="sec03 sections clearfix">
+    <div class="inner">
+      <div class="wrap">
+      <p class="tit">ライフスタイルに合わせて間取りを変更できる</p>
+      <p class="txt">ダブルオープンフレキシブルプラン</p>
+      <p class="lead">リビング・ダイニングと隣接する洋室を可動式間仕切り扉で開放的な一体空間にもプライバシーの保たれた個室にも変更可能なダブルオープンフレキシブルプランを採用しました。リビング・ダイニングとの一体感を高めるため、主開口側であるサッシュ面に余計な袖壁を作らない従来の設計に加え、主開口とは反対側からも単独で開くことができる扉を採用することで洋室のプライベート性や家具レイアウトの自由度が大きくアップしました。ライフスタイルの変化にリフォームすることなく自在に対応できます。</p>
+      <div class="images">
+        <div class="image">
+        <img src="./images/private/img02.png" alt="">
+        </div>
+        <div class="image last">
+        <img src="./images/private/img03.png" alt="">
+        </div>
+      </div>
+      </div><!-- wrap -->
+    </div><!-- inner -->
+  </section><!-- sec03 -->
+
+
 <div class="annotation-area">
-  <p class="page-annotation js-scroll wrap clearfix">※掲載の外観完成予想図?は計画段階の図面を基に描き起こしたもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。なお、周辺環境は2021年5月時点の都市データを基に描き起こしたもので、実際とは多少異なります。また、周辺環境・眺望は将来変わる場合がございます。一部敷地外（道路・河川等）を合わせて着彩しています。また周辺建物は概略ラインのみ表現しています。建物等の状況がわかりやすいよう、電柱等を透視して描いています。敷地内の樹木等はある程度生育した後をイメージして描いたものです。また、葉や花の色合い、樹形等はイメージであり実際とは異なります。なお、植栽計画は変更される場合がありますので、あらかじめご了承ください。<br>※掲載のエントランス完成予想図は計画段階の図面を基に描き起こしたものに車等をCG合成したもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。敷地内の樹木等はある程度生育した後をイメージして描いたものです。また、葉や花の色合い、樹形等はイメージであり実際とは異なります。なお、植栽計画は変更される場合がありますので、あらかじめご了承ください。<br>※掲載の外観完成予想図?は、現地15階相当から北西方面を撮影した周辺写真(2023年12月撮影）に、計画段階の図面を基に描き起こした建物完成予想図をCG合成・加工したもので、実際とは異なります。また、形状の細部および設備機器等については省略しております。周辺環境・眺望は将来変わる場合があります。また、季節・天候によっても異なる場合があります。家具・調度品等オプション仕様は販売価格に含まれません。</p></div>
+  <p class="page-annotation js-scroll wrap clearfix"></p></div>
 </main>
 
 <?php require(INCLUDES."footer.php"); ?>
