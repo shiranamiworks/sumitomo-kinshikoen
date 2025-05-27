0a94c7fb490f45d13c0279f574b1a6e7f93fd87a
yoshino umemi <umemi.0401@gmail.com>
2025-05-27 11:54:44 +0900

    no message

--- develop/structure02.php
+++ develop/structure02.php
@@ -38,6 +38,7 @@
   <section class="sections sec01 clearfix">
     <div class="wrap">
       <div class="tit">防災対策</div>
+      <div class="tit02">［共用部］</div>
       <ul class="lists list00">
         <li class="full">
           <p class="tit-img">エレベーター安全装置</p>
@@ -55,30 +56,36 @@
         <li>
           <p class="tit-img">オートドアガラス</p>
           <p class="lead">災害時などに安全に避難できるようエントランス、サブエントランスのオートドアには強化ガラスを採用しました。強化ガラスは衝撃に強く割れにくい上に、万一割れた場合でも破片の角が鋭利にならないという特徴があります。</p>
-          <p class="tit-img">屋内消火栓</p>
-          <p class="lead">火災が発生した時、初期消火を行うことができる屋内消火栓が共用部の各所に設置されています。</p>
         </li>
         <li>
           <p class="tit-img">非常用発電機</p>
           <p class="lead">地震などの災害時に万一停電しても、消火ポンプ、非常用エレベーターなどの防災負荷及び水道直結増圧ポンプ、汚水排水ポンプ、雨水排水ポンプなどに非常用発電機から一定時間電力を供給します。<span>※被災の状況により電力を供給できない場合があります。</span></p>
-          <p class="tit-img">非常用マンホールトイレ</p>
-          <p class="lead">災害時に敷地内の特定のマンホールのフタを外して簡易トイレ（非常用マンホールトイレ）を設置するだけですぐに利用できます。</p>
         </li>
         <li>
           <p class="tit-img">災害時の給水対策</p>
           <p class="lead">1階共用部の管理員用トイレ、共用水栓（ゴミ置場、ペット足洗い場）、管理事務室のミニシンクは水道本管から給水ポンプを経由せずに給水しているので、万一の停電で給水ポンプが停止しても、水を使うことができます。<span>※被災の状況により水が使えない場合があります。</span></p>
+        </li>
+        <li>
+          <p class="tit-img">屋内消火栓</p>
+          <p class="lead">火災が発生した時、初期消火を行うことができる屋内消火栓が共用部の各所に設置されています。</p>
+        </li>
+        <li>
+          <p class="tit-img">非常用マンホールトイレ</p>
+          <p class="lead">災害時に敷地内の特定のマンホールのフタを外して簡易トイレ（非常用マンホールトイレ）を設置するだけですぐに利用できます。</p>
+        </li>
+        <li>
           <p class="tit-img">防火水槽</p>
           <p class="lead">万一の火災に備えて、消防隊の迅速な消火活動をバックアップするために、地下に約40ｔの水を貯めることができる防火水槽を設置しています。</p>
         </li>
         </ul>
-
+      <div class="tit02">［専有部］</div>
       <ul class="lists list03">
         <li>
           <p class="tit-img">火災感知器</p>
           <p class="lead">万一火災などが発生した場合は、火災感知器が作動。住戸内インターホン（親機）と玄関前インターホン(子機)より警報が鳴り、室内外に異常をお知らせするとともに、警備会社、住友不動産建物サービス設備管理センターへ自動通報されます。<br><span>※10階まで。11階以上はスプリンクラーとなります。</span></p>
           <div class="image tb">
             <img src="./images/structure02/img02.png" alt="" class="">
-            <p class="annotation gothic">参考写真２</p>
+            <p class="annotation gothic">参考写真2</p>
           </div>
         </li>
         <li>
@@ -86,7 +93,7 @@
           <p class="lead">万一ガス漏れが発生した場合は、ガス漏れ検知器が作動。当該住戸内のインターホン（親機）と玄関前インターホン（子機）より警報音が鳴り、室内外に異常をお知らせするとともに、警備会社および住友不動産建物サービス設備管理センターへ自動通報されます。</p>
           <div class="image tb">
             <img src="./images/structure02/img03.png" alt="" class="">
-            <p class="annotation gothic">参考写真２</p>
+            <p class="annotation gothic">参考写真2</p>
           </div>
         </li>
         <li>
@@ -94,25 +101,25 @@
           <p class="lead">キッチンの吊戸棚、洗面化粧台の三面鏡収納の扉には、地震の揺れによって扉が開いて、収納物が落下しないように、耐震ラッチを設置。住まう方の安全に配慮しています。</p>
           <div class="image tb">
             <img src="./images/structure02/img04.png" alt="" class="">
-            <p class="annotation gothic">参考写真２</p>
+            <p class="annotation gothic">参考写真1</p>
           </div>
         </li>
         <li>
           <div class="image pc">
             <img src="./images/structure02/img02.png" alt="" class="">
-            <p class="annotation gothic">（概念図）</p>
+            <p class="annotation gothic">参考写真2</p>
           </div>
         </li>
         <li>
           <div class="image pc">
             <img src="./images/structure02/img03.png" alt="" class="">
-            <p class="annotation gothic">（概念図）</p><p class="annotation right01 gothic">（概念図）</p>
+            <p class="annotation gothic">参考写真2</p>
           </div>
         </li>
         <li>
           <div class="image pc">
             <img src="./images/structure02/img04.png" alt="" class="">
-            <p class="annotation gothic">（概念図）</p><p class="annotation right01 gothic">（概念図）</p>
+            <p class="annotation gothic">参考写真1</p>
           </div>
         </li>
         </ul>
@@ -166,7 +173,7 @@
 
 
   <div class="annotation-area">
-  <p class="page-annotation js-scroll wrap clearfix">※掲載の参考写真1は本物件と同様仕様のコンセプトルーム（総合マンションギャラリー秋葉原館）を撮影（2022年8月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれておりません。<br>※掲載の参考写真2はメーカーカタログ等の参考写真であり本物件のものではありません。※設備・仕様はタイプにより異なります。</p></div>
+  <p class="page-annotation js-scroll wrap clearfix">※掲載の参考写真1は本物件と同様仕様のコンセプトルーム（総合マンションギャラリー新橋館）を撮影（2023年1月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれておりません。<br>※掲載の参考写真2はメーカーカタログ等の参考写真であり本物件のものではありません。<br>※設備・仕様はタイプにより異なります。</p></div>
 </main>
 
 <div class="structure-link structure-link--3">
